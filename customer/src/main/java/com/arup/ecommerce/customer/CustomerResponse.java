a4a20e7f999603058fbfd255d66cfadb705e2604
arup10leo <83360443+arup10leo@users.noreply.github.com>
2026-02-01 00:07:39 +0530

    Add files via upload

---
+++ customer/src/main/java/com/arup/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,12 @@
+package com.arup.ecommerce.customer;
+
+public record CustomerResponse(
+    String id,
+    String firstname,
+    String lastname,
+    String email,
+    Address address
+) {
+
+}
+
---
+++ customer/src/main/java/com/arup/ecommerce/handler/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.arup.ecommerce.handler;
+
+import java.util.Map;
+import java.util.Set;
+
+public record ErrorResponse(
+    Map<String, String> errors
+) {
+
+}
+
